fa8dbdd31d9e413840234620d6a10f2d5fdd09ce
Pritam Neog <neog.geek123@gmail.com>
2024-08-03 12:23:36 +0530

    added dummy api call [get, getById]

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,8 @@ export default async function Page() {
     const employees = await getEmployees();
     return (
       <div className="p-8 mt-10">
-        <div className="max-w-7xl mx-auto">
+        <div className="max-w-7xl mx-auto relative">
+          <button className="absolute top-0 right-0 bg-green-500 text-white py-2 px-2 rounded mt-4">Add New Employee</button>
           <h1 className="text-3xl font-bold mb-8 mt-4">Employees</h1>
           <EmployeeList employees={employees.data} />
         </div>
--- src/util/getEmployees.js
+++ src/util/getEmployees.js
@@ -1,43 +1,51 @@
 import { unstable_noStore } from "next/cache";
+import fs from 'fs';
 
 async function handler() {
-    const params = new URLSearchParams({
-      limit: 10,
-      offset: 0,
-    });
-  
-    try {
-      unstable_noStore();
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/api/employees?${params.toString()}`,
-        {
-          method: "GET",
-          headers: {
-            projectid: process.env.NEXT_PUBLIC_COSMOCLOUD_PROJECT_ID,
-            environmentId: process.env.NEXT_PUBLIC_COSMOCLOUD_ENVIRONMENT_ID,
-          },
-        }
-      );
-  
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-  
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      return error.json();
-    }
+  const params = new URLSearchParams({
+    limit: 10,
+    offset: 0,
+  });
+
+  try {
+    unstable_noStore();
+    // Commenting out the real API call
+    // const response = await fetch(
+    //   `${process.env.NEXT_PUBLIC_API_URL}/api/employees?${params.toString()}`,
+    //   {
+    //     method: "GET",
+    //     headers: {
+    //       projectid: process.env.NEXT_PUBLIC_COSMOCLOUD_PROJECT_ID,
+    //       environmentId: process.env.NEXT_PUBLIC_COSMOCLOUD_ENVIRONMENT_ID,
+    //     },
+    //   }
+    // );
+
+    // if (!response.ok) {
+    //   throw new Error("Network response was not ok");
+    // }
+
+    // const data = await response.json();
+    // return data;
+
+    // Using dummy response from response.json
+    const fileContents = fs.readFileSync('../response.json', 'utf8');
+    const data = JSON.parse(fileContents);
+    return data;
+  } catch (error) {
+    console.error("Failed to fetch employees:", error);
+    throw error;
   }
-  
+}
+
 async function getEmployees() {
-    try {
-      const employees = await handler();
-      return employees;
-    } catch (error) {
-      console.error("Failed to fetch employees:", error);
-      throw error;
-    }
+  try {
+    const employees = await handler();
+    return employees;
+  } catch (error) {
+    console.error("Failed to fetch employees:", error);
+    throw error;
   }
+}
 
 export default getEmployees;
